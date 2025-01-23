3a78875a5d4b1d9e537174a787566c05c727dda4
Dhanush Govindaraju <35737348+dhanushdragneel@users.noreply.github.com>
2025-01-23 10:20:28 +0100

    Delete spring-boot-hello-world-to-Quarkus directory

--- spring-boot-hello-world-to-Quarkus/src/main/java/com/example/helloworld/QuarkusHelloWorldApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot;
-
-import io.quarkus.runtime.Quarkus;
-
-public class QuarkusHelloWorldApplication {
-    public static void main(String... args) {
-        Quarkus.run(args);
-    }
-}
