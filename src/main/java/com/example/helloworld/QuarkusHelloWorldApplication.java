5513eaf6e2309d3358a173789bb3e142e167142d
Dhanush Govindaraju <35737348+dhanushdragneel@users.noreply.github.com>
2025-01-23 10:21:23 +0100

    Add files via upload

---
+++ src/main/java/com/example/helloworld/QuarkusHelloWorldApplication.java
@@ -0,0 +1,9 @@
+package com.example.springboot;
+
+import io.quarkus.runtime.Quarkus;
+
+public class QuarkusHelloWorldApplication {
+    public static void main(String... args) {
+        Quarkus.run(args);
+    }
+}
